ba8f8a5c24f82b07528a221220801151efddef0d
Iyera-rgb <arjun.iyer.20@gmail.com>
2022-09-02 14:57:30 +0530

    Version 11

--- src/NewHotline.java
+++ src/NewHotline.java
@@ -1,72 +1,76 @@
-import java.sql.SQLOutput;
 import java.util.*;
 import java.lang.*;
-
 
 public class NewHotline {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Please input the hotline you would like to contact");
-        String HotlineInput = sc.nextLine();
-        String Term = "";
-//convert all the characters in the string to lowercase, so that the index value makes it easier to use the index
+
+        System.out.print("Enter a string: ");
+        String HotlineInput = "";
+        HotlineInput = sc.nextLine();
+//Make all lowercase for conversion to be quick
         HotlineInput = HotlineInput.toLowerCase();
-        char[] c = HotlineInput.toCharArray();
-        char[] Final_Array = Term.toCharArray();
-        String newStr = "";
-//create an index for the char character inputted: If a character is taken from the list assign a number to it using
-//Assign the values from the letters using the hotline and for the numbers just add the numbers to the string normally
-        char[] A = newStr.toCharArray();
-        String Output = "";
-//remove the space from the string, The integer for Space is s
-        for (int Space = 0; Space < c.length; Space++) {
-            if (c[Space] != ' ') {
-                Term = Term + c[Space];
+        char[] Term = HotlineInput.toCharArray();
+
+        String temp = "";
+
+        for (int Space = 0; Space < Term.length; Space++) {
+            if (Term[Space] != ' ') {
+                temp = temp + Term[Space];
             }
         }
-// if there are values less that are 7 in total add 0s
-        if (Final_Array.length > 7) {
+
+        char[] Replacement = temp.toCharArray();
+        String newStr = "";
+
+        if (Replacement.length > 7) {
             for (int LessThan = 0; LessThan < 7; LessThan++) {
-                newStr += Final_Array[LessThan];
+                newStr += Replacement[LessThan];
             }
-//if there are more than 7 values remove the values until there are 7
-        } else if (Final_Array.length < 7) {
-            for (int GreaterThan = 0; GreaterThan < Final_Array.length; GreaterThan++) {
-                newStr += Final_Array[GreaterThan];
+        } else if (Replacement.length < 7) {
+            for (int GreaterThan = 0; GreaterThan < Replacement.length; GreaterThan++) {
+                newStr += Replacement[GreaterThan];
             }
-            int Amountof0s = 7 - Final_Array.length;
-            for (int Subtract = 0; Subtract < Amountof0s; Subtract++) {
+
+            int Count0 = 7 - Replacement.length;
+            for (int EqualTo = 0; EqualTo < Count0; EqualTo++) {
                 newStr += "0";
             }
-        } else if (Final_Array.length == 7) {
-            for (int Equals = 0; Equals < Final_Array.length; Equals++) {
-                newStr += Final_Array[Equals];
+        } else if (Replacement.length == 7) {
+            for (int SIU = 0; SIU < Replacement.length; SIU++) {
+                newStr += Replacement[SIU];
             }
         }
-        for (int i = 0; i < A.length; i++) {
-            if (A[i] == 'a' || A[i] == 'b' || A[i] == 'c') {
-                Output = Output + "2";
-            } else if (A[i] == 'd' || A[i] == 'e' || A[i] == 'f') {
-                Output = Output + "3";
-            } else if (A[i] == 'g' || A[i] == 'h' || A[i] == 'i') {
-                Output = Output + "4";
-            } else if (A[i] == 'j' || A[i] == 'k' || A[i] == 'l') {
-                Output = Output + '5';
-            } else if (A[i] == 'm' || A[i] == 'n' || A[i] == 'o') {
-                Output = Output + "6";
-            } else if (A[i] == 'p' || A[i] == 'q' || A[i] == 'r' || A[i] == 's') {
-                Output = Output + "7";
-            } else if (A[i] == 't' || A[i] == 'u' || A[i] == 'v') {
-                Output = Output + "8";
-            } else if (A[i] == 'w' || A[i] == 'x' || A[i] == 'y' || A[i] == 'z') {
-                Output = Output + '9';
-            } else if (A[i] == '0' || A[i] == '1' || A[i] == '2' || A[i] == '3' || A[i] == '4' || A[i] == '5' || A[i] == '6' || A[i] == '7' || A[i] == '8' || A[i] == '9') {
-                Output = Output + A[i];
+
+        char[] InsertedLetter = newStr.toCharArray();
+        String OutputNumber = "";
+
+        for (int i = 0; i < InsertedLetter.length; i++) {
+            if (InsertedLetter[i] == 'a' || InsertedLetter[i] == 'b' || InsertedLetter[i] == 'c') {
+                OutputNumber = OutputNumber + "2";
+            } else if (InsertedLetter[i] == 'd' || InsertedLetter[i] == 'e' || InsertedLetter[i] == 'f') {
+                OutputNumber = OutputNumber + "3";
+            } else if (InsertedLetter[i] == 'g' || InsertedLetter[i] == 'h' || InsertedLetter[i] == 'i') {
+                OutputNumber = OutputNumber + "4";
+            } else if (InsertedLetter[i] == 'j' || InsertedLetter[i] == 'k' || InsertedLetter[i] == 'l') {
+                OutputNumber = OutputNumber + "5";
+            } else if (InsertedLetter[i] == 'm' || InsertedLetter[i] == 'n' || InsertedLetter[i] == 'o') {
+                OutputNumber = OutputNumber + "6";
+            } else if (InsertedLetter[i] == 'p' || InsertedLetter[i] == 'q' || InsertedLetter[i] == 'r' || InsertedLetter[i] == 's') {
+                OutputNumber = OutputNumber + "7";
+            } else if (InsertedLetter[i] == 't' || InsertedLetter[i] == 'u' || InsertedLetter[i] == 'v') {
+                OutputNumber = OutputNumber + "8";
+            } else if (InsertedLetter[i] == 'w' || InsertedLetter[i] == 'x' || InsertedLetter[i] == 'y' || InsertedLetter[i] == 'z') {
+                OutputNumber = OutputNumber + "9";
+            } else if (InsertedLetter[i] == '0' || InsertedLetter[i] == '1' || InsertedLetter[i] == '2' || InsertedLetter[i] == '3' || InsertedLetter[i] == '4' || InsertedLetter[i] == '5' || InsertedLetter[i] == '6' || InsertedLetter[i] == '7' || InsertedLetter[i] == '8' || InsertedLetter[i] == '9') {
+                OutputNumber = OutputNumber + InsertedLetter[i];
             }
         }
-        String Split0 = Output.substring(0, 6);
-        String Split1 = Output.substring(6);
-        System.out.println("The Toll Number you are looking for is: 1-800-" + Split0 + Split1);
+
+        String PART1 = OutputNumber.substring(0,2);//not right
+        String PART2 = OutputNumber.substring(2);//not right
+
+        System.out.println(PART1 +"-" +PART2);
+
     }
 }
-
